54db019286c0926916a54555d1937ec3d923b619
Nancy1226 <nancy-12_03@hotmail.com>
2023-06-27 22:45:05 -0600

    first commit

--- package.api.js
+++ package.api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export  const createPackage = async(pkg)=>{
-    return await axios.post("http://localhost:3000/package/create", pkg);
+    return await axios.post("http://52.205.142.120:3000/package/create", pkg);
 }
